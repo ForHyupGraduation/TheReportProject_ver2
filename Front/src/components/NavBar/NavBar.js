496a020ffd082e08124820229897443fa8aef403
Picka <marmong@naver.com>
2023-02-14 21:30:01 +0900

    Update NavBar.js

--- Front/src/components/NavBar/NavBar.js
+++ Front/src/components/NavBar/NavBar.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   const logOut = () => {
     sessionStorage.clear();
-    window.location.reload();
+    window.location = "http://localhost:3000/";
   };
 
   return (
