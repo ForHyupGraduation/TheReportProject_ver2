2ca82d7cef33775b59b44fdb23970de7820b2486
Chae-1 <coguddlf1000@naver.com>
2023-02-20 21:43:45 +0900

    tset

--- back/src/main/java/back/back/repository/PortFolioRepository.java
+++ back/src/main/java/back/back/repository/PortFolioRepository.java
@@ -2,10 +2,13 @@ package back.back.repository;
 
 import back.back.domain.PortFolio;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PortFolioRepository extends JpaRepository<PortFolio, Long> {
     List<PortFolio> findByMemberId(Long MemberId);
-    PortFolio removeByCompanyName(String companyName);
+    @Query("delete from PortFolio p where p.companyName = :companyName")
+    PortFolio removeByCompanyName(@Param("companyName") String companyName);
 }
