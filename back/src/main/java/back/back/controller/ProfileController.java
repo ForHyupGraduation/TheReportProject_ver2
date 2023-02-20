1c6b0bba19bfb8edeeff0c29953dcf71717c9d8a
Chae-1 <coguddlf1000@naver.com>
2023-02-20 19:41:03 +0900

    asd

--- back/src/main/java/back/back/controller/ProfileController.java
+++ back/src/main/java/back/back/controller/ProfileController.java
@@ -18,7 +18,7 @@ public class ProfileController {
     private final ProfileService profileService;
     private final MemberService memberService;
 
-    @PostMapping("/add/portpolio")
+    @PostMapping("/add/portfolio")
     public MyProfileDto addPortPolio(@RequestBody PortFolioParam param) {
         // 1. memberId가 넘어 오면 회원 정보를 조회한다.
         Member member = profileService.addPortPolio(param.getMemberId(), param.getCompanyName());
