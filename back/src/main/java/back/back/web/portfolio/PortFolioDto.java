c0ae033392da5c90f61a13f375ff7948d3bfee6e
Chae-1 <coguddlf1000@naver.com>
2023-02-20 17:20:08 +0900

    asd

--- back/src/main/java/back/back/web/portfolio/PortFolioDto.java
+++ back/src/main/java/back/back/web/portfolio/PortFolioDto.java
@@ -7,12 +7,11 @@ import java.util.List;
 public class PortFolioDto {
     private String categoryName;
     private String companyName;
-    private Integer growthPoint;
-
+    private Integer interestPoint;
 
     public PortFolioDto(PortFolio portFolio) {
         this.categoryName = portFolio.getCateGoryName();
         this.companyName = portFolio.getCompanyName();
-        this.growthPoint = portFolio.getInterestPoint();
+        this.interestPoint = portFolio.getInterestPoint();
     }
 }
