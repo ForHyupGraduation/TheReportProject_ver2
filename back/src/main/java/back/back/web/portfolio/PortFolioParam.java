c53bb441809fcb61cb93d6d0a5aa83982b89a653
Chae-1 <coguddlf1000@naver.com>
2023-02-20 18:51:01 +0900

    asd

--- back/src/main/java/back/back/web/portfolio/PortFolioParam.java
+++ back/src/main/java/back/back/web/portfolio/PortFolioParam.java
@@ -8,4 +8,9 @@ import lombok.Setter;
 public class PortFolioParam {
     private Long memberId;
     private String companyName;
+
+    public PortFolioParam(Long memberId, String companyName) {
+        this.memberId = memberId;
+        this.companyName = companyName;
+    }
 }
